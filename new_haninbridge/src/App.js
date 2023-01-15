196d33044e4d4dc9fc6c50029d6a372ce97aad20
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-15 22:23:31 +0900

    Routing

--- new_haninbridge/src/App.js
+++ new_haninbridge/src/App.js
@@ -1,6 +1,15 @@
+import { BrowserRouter as Router, Routes, Route }from 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path={`/`} element={<Landing />}></Route>
+          <Route path={`/aboutus`} element={<Landing />}></Route>
+          <Route path={`/interpretation`} element={<Landing />}></Route>
+          <Route path={`/consulting`} element={<Landing />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
