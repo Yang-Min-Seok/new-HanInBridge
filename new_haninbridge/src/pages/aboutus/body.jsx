78d979e7e396b4dcc5dd3a9c1a642219e92122e4
Yang-Min-Seok <83502596+Yang-Min-Seok@users.noreply.github.com>
2023-01-19 00:29:50 +0900

    # Aboutus implement * without slide

--- new_haninbridge/src/pages/aboutus/body.jsx
+++ new_haninbridge/src/pages/aboutus/body.jsx
@@ -1,8 +1,60 @@
+import { BodyDiv, IntroBox, IntroUpTitle, IntroDownTitle, IntroContentBox, IntroContent, PartnerBox, PartnerTitle, PartnerSlide, DirectorBox, DirectorTitle, DirectorContentBox, DirectorImg, DirectorNameAndContentBox, DirectorName, DirectorContent } from "./style";
 function Body() {
     return (
-        <div>
-            <h1>About us Body</h1>
-        </div>
+        <BodyDiv>
+            <IntroBox>
+                <IntroUpTitle>Hanin Bridge</IntroUpTitle>
+                <IntroDownTitle>한인브릿지</IntroDownTitle>
+                <IntroContentBox>
+                    <IntroContent>
+                        한인브릿지는 한국 기업들이 인도네시아로 진출할 수 있도록<br/>
+                        도움을 주고자하는 사업을 하고 있습니다.<br/>
+                        각종 수출입 계약에 관한 서류 번역부터,<br/>
+                        수준 높은 수출 상담회 통역,<br/>
+                        더 나아가 현지에서 열리는 전시회 참가를 함으로써<br/>
+                        인도네시아 진출을 어려워하는 한국 기업이 <br/>
+                        더욱 원활하게 인도네시아로 나아갈 수 있도록 다리 역할을합니다.<br/>
+                        한인브릿지와 함께 인도네시아로 나아가<br/>
+                        인도네시아와 한국이 보다 쉽게 연결될 수 있다는 것을 경험할 수 있습니다.<br/>
+                    </IntroContent>
+                </IntroContentBox>
+            </IntroBox>
+            <PartnerBox>
+                <PartnerTitle>Our Partners</PartnerTitle>
+                <PartnerSlide>
+
+                </PartnerSlide>
+            </PartnerBox>
+            <DirectorBox>
+                <DirectorTitle>Director</DirectorTitle>
+                <DirectorContentBox>
+                    <DirectorImg></DirectorImg>
+                    <DirectorNameAndContentBox>
+                        <DirectorName>Jin Woo Park</DirectorName>
+                        <DirectorContent>
+                            ・ 한국외국어대학교 말레이-인도네시아어과 졸업 <br/>
+                            ・ 한인브릿지 대표<br/>
+                            ・ 기업경력<br/>
+                            - KCC글라스 공장 가동 메뉴얼 인도네시아어 번역<br/>
+                            - 각종 RPG 모바일 게임 인도네시아어 번역<br/>
+                            - 한국무역협회 개최 온라인 수출 상담회 통역<br/>
+                            ・ 개인경력<br/>
+                            - KBS World Radio Indonesia 고정 출연 DJ<br/>
+                            - 조코 위도도 대통령 청계천 행사 인도네시아어 통역<br/>
+                            - 대기업/중소기업 인도네시아어 기업 강사<br/>
+                            - SBS/Net.TV 예능 ‘Good Friends’ 통역 PD<br/>
+                            - 뷰티웰팜 의학 세미나 담당 지정 통역<br/>
+                            - 한국무역협회 지정 인도네시아어 통역사<br/>
+                            - MBC, SBS, KBS, JTBC 인도네시아어 자막 번역<br/>
+                            - 미래창조과학부 ‘한국정부와 인도네시앙 농.어촌 브로드밴드 네트워크 구축 산업’ 현장 통역<br/>
+                            - JETEMA 의학 세미나 지정 인도네시아어 통역사<br/>
+                            - SBS 정글의 법칙 ‘코모도 편’ 인도네시아어 통역 및 자막 번역<br/>
+                            - ISWAM 의학 세미나 인도네시아어 통역 및 자막 번역<br/>
+                        </DirectorContent>
+                    </DirectorNameAndContentBox>
+                </DirectorContentBox>
+            </DirectorBox>
+        </BodyDiv>
     )
 }
 
---
+++ new_haninbridge/src/pages/aboutus/style.jsx
@@ -0,0 +1,130 @@
+import styled from "styled-components";
+import CeoImg from "../../resources/images/aboutUs/Director/director.jpeg"
+export const BodyDiv = styled.div`
+    
+`;
+
+export const IntroBox = styled.div`
+    
+`;
+
+export const IntroUpTitle = styled.h1`
+    margin: 0;
+    font-size: 70px;
+    font-weight: 100;
+    letter-spacing: 15px;
+    text-align: center;
+    line-height: 3;
+`;
+
+export const IntroDownTitle = styled.h1`
+    margin: 0;
+    font-size: 55px;
+    font-weight: 700;
+    letter-spacing: 15px;
+    text-align: center;
+    line-height: 1;
+`;
+
+export const IntroContentBox = styled.div`
+    width:90%;
+    margin: 0 auto;
+    margin-top: 50px;
+    border: 3px solid #A67951;
+    box-sizing: border-box;
+    padding: 50px;
+
+    position: relative;
+    & ::before{
+        content:"";
+        background-image: url("https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycGxhbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60");
+        position: absolute;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        opacity:0.3;
+    }
+`;
+
+export const IntroContent = styled.p`
+    font-size: 30px;
+    line-height: 2.5;
+    font-weight: 900;
+    font-family: 'Times New Roman', Times, serif;
+    text-align: right;
+`;
+
+export const PartnerBox = styled.div`
+    
+`;
+export const PartnerTitle = styled.h1`
+    margin: 0;
+    font-size: 70px;
+    font-weight: 100;
+    letter-spacing: 10px;
+    text-align: center;
+    line-height: 3;
+`;
+
+export const PartnerSlide = styled.div`
+    width: 90%;
+    height: 600px;
+    margin: 0 auto;
+    border: 3px solid #A67951;
+    box-sizing: border-box;
+
+`;
+
+export const DirectorBox = styled.div`
+    width:90%;
+    margin: 0 auto;
+    margin-bottom: 50px;
+`;
+
+export const DirectorTitle = styled.h1`
+    margin: 0;
+    font-size: 70px;
+    font-weight: 100;
+    letter-spacing: 10px;
+    text-align: center;
+    line-height: 3;
+`;
+export const DirectorContentBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const DirectorImg = styled.div`
+    width: 45%;
+    background-image: url(${CeoImg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+`;
+
+export const DirectorNameAndContentBox = styled.div`
+    width: 45%;
+`;
+
+export const DirectorName = styled.h1`
+    margin: 0 auto;
+    margin-bottom: 20px;
+    text-align: center;
+    font-size: 50px;
+    font-weight: 100;
+    line-height: 2;
+    border-bottom: 3px solid #A67951;
+    box-sizing: border-box;;
+`;
+
+export const DirectorContent = styled.p`
+    margin: 0;
+    font-size: 18px;
+    font-family: 'Times New Roman', Times, serif;
+    font-weight: 500;
+    line-height: 2;
+`;
